954588312f85ef0adaeb04e3d0adbf0fb058b3d9
djkreddys-cmyk <djkreddys@gmail.com>
2026-02-17 13:09:21 +0530

    Update qr-server.js

--- qr-server.js
+++ qr-server.js
@@ -66,6 +66,9 @@ app.get('/api/projects',auth,(req,res)=>{
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
 
 
