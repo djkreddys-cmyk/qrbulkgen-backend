c6d70527b7bbc1acfe00d0278fb8de5740f1fc65
Jaswanth <djkreddys@gmail.com>
2026-02-24 06:17:12 +0530

    Add dynamic QR routes

--- index.js
+++ index.js
@@ -21,21 +21,6 @@ fastify.post("/users", async (request, reply) => {
   return user
 })
 
-const start = async () => {
-  try {
-    await fastify.listen({
-      port: process.env.PORT || 3000,
-      host: "0.0.0.0"
-    })
-
-    console.log("Server running 🚀")
-  } catch (err) {
-    fastify.log.error(err)
-    process.exit(1)
-  }
-}
-
-start()
 
 // Create user
 fastify.get("/users", async () => {
@@ -68,3 +53,70 @@ fastify.put("/users/:id", async (request) => {
 
   return updatedUser
 })
+
+const QRCodeLib = require("qrcode")
+const { nanoid } = require("nanoid")
+
+//Generate Dynamic QR
+fastify.post("/generate-dynamic-qr", async (request) => {
+  const { type, destination } = request.body
+
+  if (!destination) {
+    return { error: "Destination is required" }
+  }
+
+  const shortCode = nanoid(6)
+
+  await prisma.qRCode.create({
+    data: {
+      type,
+      shortCode,
+      destination
+    }
+  })
+
+  const shortUrl = `https://qrbulkgen-backend-production.up.railway.app/s/${shortCode}`
+
+  const qrImage = await QRCodeLib.toDataURL(shortUrl)
+
+  return {
+    qr: qrImage,
+    shortUrl
+  }
+})
+// Redirect Route (Very Important)
+fastify.get("/s/:code", async (request, reply) => {
+  const { code } = request.params
+
+  const record = await prisma.qRCode.findUnique({
+    where: { shortCode: code }
+  })
+
+  if (!record) {
+    return reply.code(404).send({ message: "QR not found" })
+  }
+
+  // Increment scan count
+  await prisma.qRCode.update({
+    where: { shortCode: code },
+    data: { scanCount: { increment: 1 } }
+  })
+
+  return reply.redirect(record.destination)
+})
+
+const start = async () => {
+  try {
+    await fastify.listen({
+      port: process.env.PORT || 3000,
+      host: "0.0.0.0"
+    })
+
+    console.log("Server running 🚀")
+  } catch (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
+}
+
+start()
